74ce22b6b3ce1681c4d2b7bffdf308ee903be5ce
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2016-02-11 22:01:50 +0200

    Updated the selectors for Instagram, since they have changed since last release.

--- page_objects/pages/explore_tags_page.rb
+++ page_objects/pages/explore_tags_page.rb
@@ -3,12 +3,12 @@ module PageObjects
     class ExploreTagsPage < SitePrism::Page
       set_url "#{Settings.instagram_root_url}/explore/tags{/hash_tag}/"
 
-      elements :posts, 'a.-cx-PRIVATE-PostsGridItem__root.-cx-PRIVATE-PostsGrid__item'
+      elements :posts, '._myci9 a'
       elements :likes,  'a.coreSpriteHeartOpen'
-      elements :comment_forms, 'form.-cx-PRIVATE-PostInfo__commentCreator'
-      elements :comments, ".-cx-PRIVATE-PostInfo__commentCreatorInput"
+      elements :comment_forms, 'form._k3t69'
+      elements :comments, "._7uiwk"
 
-      element :next_post, 'a.-cx-PRIVATE-PostModalConsumer__rightArrow'
+      element :next_post, 'a._de018.coreSpriteRightPaginationArrow'
     end
   end
 end
--- page_objects/pages/instagram_page.rb
+++ page_objects/pages/instagram_page.rb
@@ -3,9 +3,10 @@ module PageObjects
     class InstagramPage < SitePrism::Page
       set_url Settings.instagram_root_url
 
-      element :username, "[name='username']"
-      element :password, "[name='password']"
-      element :login,    "button[data-reactid='.0.1.0.1.0.1.0.2']"
+      element :login_link, "a._k6cv7"
+      element :username,   "[name='username']"
+      element :password,   "[name='password']"
+      element :login,      "button[data-reactid='.0.1.0.1.0.1.2.2']"
 
       element :search_input, "input[data-reactid='.0.2.0.1.$searchBox.0']"
     end
